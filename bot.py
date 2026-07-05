43b7f55c064dbcf21f56f66b6c964d735a82a2b7
ronvizli064-max <ronvizli064@gmail.com>
2026-07-05 19:24:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,11 @@
 import telebot
 
-TOKEN = "ТУТ_ВСТАВ_СВІЙ_ТОКЕН"
+TOKEN = @BotFather
 
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привіт! Натисни /buy щоб купити доступ ⭐")
-
-@bot.message_handler(commands=['buy'])
-def buy(message):
-    bot.send_message(message.chat.id, "Тут буде оплата (далі додамо ⭐)")
+    bot.send_message(message.chat.id, "Бот працює 🚀")
 
 bot.polling()
